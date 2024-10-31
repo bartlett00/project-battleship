8640225380555f97ec179efa094930c8a7300da4
Pax <113464089+bartlett00@users.noreply.github.com>
2024-10-31 09:51:46 -0500

    Add basic webpack setup to manage the DOM.

--- src/game.js
+++ src/game.js
@@ -2,3 +2,6 @@ import Ship from "./ship";
 import GameBoard from "./gameboard";
 import Player from "./player";
 import createShipList from "./create-ship-list";
+import "./styles.css";
+
+console.log("test");
